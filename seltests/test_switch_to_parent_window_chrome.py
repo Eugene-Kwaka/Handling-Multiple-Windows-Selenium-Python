516c84ffce8f519a23a35bcbecfff51771408abe
Eugene-Kwaka <kwakaeugene@gmail.com>
2022-09-02 05:04:36 -0500

    Print output issue

--- seltests/test_switch_to_parent_window_chrome.py
+++ seltests/test_switch_to_parent_window_chrome.py
@@ -1,19 +1,18 @@
-from multiprocessing.connection import wait
 import pytest
 from selenium import webdriver
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options as ChromeOptions
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
-def test_single_window_chrome():
+def test_switch_to_parent_window_chrome():
     options = ChromeOptions()
     options.browser_version = '103.0'
     options.platform_name = 'Windows 10'
     lt_options = {}
     lt_options['username'] = '<username>'
     lt_options['accesskey'] = '<accesskey>'
-    lt_options['project'] = 'SingleWindowHandling'
+    lt_options['project'] = 'Switch Back To Parent Window Test'
     lt_options['selenium_version'] = '4.0.0'
     lt_options['w3c'] = True
     options.set_capability('LT:options', lt_options)
@@ -31,19 +30,33 @@ def test_single_window_chrome():
     element.click()
     # Get the window handle Id of the current parent window
     parent_guid = driver.current_window_handle
-    print(f"The parent window guid is: {parent_guid}")
-    twitter_element = WebDriverWait(driver, 3).until(EC.element_to_be_clickable((By.CSS_SELECTOR, 'a[title="Follow @Lambdatesting on Twitter"]')))
-    twitter_element.click()
+    print(f"The Parent Window guid is: {parent_guid}")
+    # Locate and click on the button "Follow Twitter & Facebook"
+    web_element = WebDriverWait(driver, 3).until(EC.element_to_be_clickable((By.ID, 'followboth')))
+    web_element.click()
     # Get the window handle ids of all the windows opened
     all_guid = driver.window_handles
     # iterate through the guids and if there is a parent window id skip it and switch to the new window
     for guid in all_guid:
         if guid != parent_guid:
             driver.switch_to.window(guid)
-            print(f"The child guid is: {guid}")
-            driver.close()
+            page_title = driver.title
+            print(f"The page title of child window is: {page_title}")
+            if page_title == "Twitter":
+                twitter_signup = WebDriverWait(driver, 2).until(EC.element_to_be_clickable((By.CSS_SELECTOR, 'a[href="/i/flow/signup"]')))
+                twitter_signup.click()
+                driver.close()
+            elif page_title == "Facebook":
+                fb_login = WebDriverWait(driver, 2).until(EC.element_to_be_clickable((By.CSS_SELECTOR, 'div[aria-label="Accessible login button"]')))
+                fb_login.click()
+                driver.close()
             break
 
+    # Switch back to parent window
+    driver.switch_to.window(parent_guid)
+    print(f"The parent guid is: {parent_guid}")
     driver.quit()
+    
+
 
   
